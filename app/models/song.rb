38f0eb46e98b318bde13daa46d6e86f8827d27e0
Adam Finch <atyreefinch@gmail.com>
2019-10-14 16:54:54 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def concat_name_song
+    "#{self.artist_name} - #{self.title}"
+  end
+
 end
